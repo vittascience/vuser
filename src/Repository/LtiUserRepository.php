76dec6a48ba4496d6a562f5c6afcf2c4fc95c98c
ultimecreation <naserbechata@gmail.com>
2023-01-06 09:21:47 +0100

    added methods to LtiUserRepository

--- src/Repository/LtiUserRepository.php
+++ src/Repository/LtiUserRepository.php
@@ -3,7 +3,41 @@
 namespace User\Repository;
 
 use Doctrine\ORM\EntityRepository;
+use User\Entity\LtiUser;
+use User\Entity\User;
 
 class LtiUserRepository extends EntityRepository{
-    
+    public function getTeachersIdByConsumer($consumer){
+       
+        $queryBuilder = $this->getEntityManager()->createQueryBuilder();
+        $results = $queryBuilder->select('lu')
+                        ->from(LtiUser::class,'lu')
+                        ->andWhere('lu.isTeacher=1')
+                        ->andWhere('lu.ltiConsumer= :consumer')
+                        ->setParameter('consumer',$consumer)
+                        ->getQuery()
+                        ->getResult();
+        $teachersId = [];
+        foreach($results as $result){
+            array_push($teachersId, $result->getUser()->getId());
+        }
+        return $results;
+    }
+
+    public function getStudentsIdByConsumer($consumer){
+       
+        $queryBuilder = $this->getEntityManager()->createQueryBuilder();
+        $results = $queryBuilder->select('lu')
+                        ->from(LtiUser::class,'lu')
+                        ->andWhere('lu.isTeacher=0')
+                        ->andWhere('lu.ltiConsumer= :consumer')
+                        ->setParameter('consumer',$consumer)
+                        ->getQuery()
+                        ->getResult();
+        $studentsId = [];
+        foreach($results as $result){
+            array_push($studentsId, $result->getUser()->getId());
+        }
+        return $results;
+    }
 }
